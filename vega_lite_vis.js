9e23f4747bbd3225fc6391f6e8b47364e88b71e9
haanhoang <94729654+haanhoang@users.noreply.github.com>
2023-10-10 17:50:14 +1100

    Edited data, fixed line charts.

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,3 +1,7 @@
 var rank_country = "bar_chart_rank.json";
+var line_state = "line_chart_state.json";
+var line_type = "line_chart_type.json";
 
 vegaEmbed("#rank_country", rank_country).then(function(result) {}).catch(console.error);
+vegaEmbed("#line_state", line_state).then(function(result) {}).catch(console.error);
+vegaEmbed("#line_type", line_type).then(function(result) {}).catch(console.error);
