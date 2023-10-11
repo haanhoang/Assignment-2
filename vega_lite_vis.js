a72f76f11c8c5fcbb83ef5b798be33ce58a59f48
haanhoang <94729654+haanhoang@users.noreply.github.com>
2023-10-11 12:11:16 +1100

    Deleted null values.

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,9 +1,11 @@
 var rank_country = "bar_chart_rank.json";
 var line_state = "line_chart_state.json";
 var line_type = "line_chart_type.json";
-var scatterplot_expenditure = "scatterplot_expenditure.json"
+var scatterplot_expenditure = "scatterplot_expenditure.json";
+var map_lga = "map_lga.json";
 
 vegaEmbed("#rank_country", rank_country).then(function(result) {}).catch(console.error);
 vegaEmbed("#line_state", line_state).then(function(result) {}).catch(console.error);
 vegaEmbed("#line_type", line_type).then(function(result) {}).catch(console.error);
 vegaEmbed("#scatterplot_expenditure", scatterplot_expenditure).then(function(result) {}).catch(console.error);
+vegaEmbed("#map_lga", map_lga).then(function(result) {}).catch(console.error);
