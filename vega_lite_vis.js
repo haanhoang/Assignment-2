071b5a58651f437e161f568845b401787ac6eb43
haanhoang <94729654+haanhoang@users.noreply.github.com>
2023-10-11 23:31:41 +1100

    Fixed a lot of stuff.

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -3,11 +3,11 @@ var line_state = "line_chart_state.json";
 var line_type = "line_chart_type.json";
 var scatterplot_expenditure = "scatterplot_expenditure.json";
 var map_lga = "map_lga.json";
-//var scatterplot_map_combined = "scatterplot_map_combined.json"
+var scatterplot_map_combined = "scatterplot_map_combined.json"
 
 vegaEmbed("#rank_country", rank_country).then(function(result) {}).catch(console.error);
 vegaEmbed("#line_state", line_state).then(function(result) {}).catch(console.error);
 vegaEmbed("#line_type", line_type).then(function(result) {}).catch(console.error);
 vegaEmbed("#scatterplot_expenditure", scatterplot_expenditure).then(function(result) {}).catch(console.error);
 vegaEmbed("#map_lga", map_lga).then(function(result) {}).catch(console.error);
-//vegaEmbed("#scatterplot_map_combined", scatterplot_map_combined).then(function(result) {}).catch(console.error);
+vegaEmbed("#scatterplot_map_combined", scatterplot_map_combined).then(function(result) {}).catch(console.error);
