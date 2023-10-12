c93a230ef394ab7d645efb98ff1c22bfc2054452
haanhoang <94729654+haanhoang@users.noreply.github.com>
2023-10-13 00:09:26 +1100

    Added new chart.

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -3,11 +3,16 @@ var line_state = "line_chart_state.json";
 var line_type = "line_chart_type.json";
 var scatterplot_expenditure = "scatterplot_expenditure.json";
 var map_lga = "map_lga.json";
-var scatterplot_map_combined = "scatterplot_map_combined.json"
+//var scatterplot_map_combined = "scatterplot_map_combined.json"
+var slope_chart = "slope_chart.json"
+var dual_line_chart = "dual_line_chart.json"
+
 
 vegaEmbed("#rank_country", rank_country).then(function(result) {}).catch(console.error);
 vegaEmbed("#line_state", line_state).then(function(result) {}).catch(console.error);
 vegaEmbed("#line_type", line_type).then(function(result) {}).catch(console.error);
 vegaEmbed("#scatterplot_expenditure", scatterplot_expenditure).then(function(result) {}).catch(console.error);
 vegaEmbed("#map_lga", map_lga).then(function(result) {}).catch(console.error);
-vegaEmbed("#scatterplot_map_combined", scatterplot_map_combined).then(function(result) {}).catch(console.error);
+//vegaEmbed("#scatterplot_map_combined", scatterplot_map_combined).then(function(result) {}).catch(console.error);
+vegaEmbed("#slope_chart", slope_chart).then(function(result) {}).catch(console.error);
+vegaEmbed("#dual_line_chart", dual_line_chart).then(function(result) {}).catch(console.error);
