456484fef9e1aaf85172ccc40ce8830af8f9ac63
haanhoang <94729654+haanhoang@users.noreply.github.com>
2023-10-13 18:14:54 +1100

    Fixed colors for 'color blind' people.

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -4,9 +4,10 @@ var line_type = "line_chart_type.json";
 var scatterplot_expenditure = "scatterplot_expenditure.json";
 var map_lga = "map_lga.json";
 //var scatterplot_map_combined = "scatterplot_map_combined.json"
-var slope_chart = "slope_chart.json"
-var dual_line_chart = "dual_line_chart.json"
-
+var slope_chart = "slope_chart.json";
+var dual_line_chart = "dual_line_chart.json";
+var pie_1 = "pie_responsible.json";
+var pie_2 = "pie_adolescent.json";
 
 vegaEmbed("#rank_country", rank_country).then(function(result) {}).catch(console.error);
 vegaEmbed("#line_state", line_state).then(function(result) {}).catch(console.error);
@@ -16,3 +17,5 @@ vegaEmbed("#map_lga", map_lga).then(function(result) {}).catch(console.error);
 //vegaEmbed("#scatterplot_map_combined", scatterplot_map_combined).then(function(result) {}).catch(console.error);
 vegaEmbed("#slope_chart", slope_chart).then(function(result) {}).catch(console.error);
 vegaEmbed("#dual_line_chart", dual_line_chart).then(function(result) {}).catch(console.error);
+vegaEmbed("#pie_1", pie_1).then(function(result) {}).catch(console.error);
+vegaEmbed("#pie_2", pie_2).then(function(result) {}).catch(console.error);
